f3963e63c4bf14988f3ca8a86349a290112815d8
trikhonghu <113248981+trikhonghu@users.noreply.github.com>
2024-02-25 19:15:52 +0700

    test_successfully_on_windows_but_not_linux

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from uart import *
 
 AIO_FEED_ID = ["nutnhan1", "nutnhan2"]
 AIO_USERNAME = "vantri15042003"
-AIO_KEY = "aio_tpKv04J1CNdPIFdhGrP2IZlP0q42"
+AIO_KEY = "aio_WKOW856DteO3E7qSDvg0h9QJ6OHR"
 
 def connected(client):
     print("Ket noi thanh cong ...")
